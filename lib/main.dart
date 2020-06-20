822044af29ed6167a99f53659926b6bd6bada79f
Djenane <djenane@live.com>
2020-06-20 15:06:59 +0400

    Flutter 8 Ball Magic answer - sample app - Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MagicBall());
+
+class MagicBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[300],
+        appBar: AppBar(
+          title: Text('Ask Me Anything !'),
+          backgroundColor: Colors.blue[700],
+        ),
+        body: MagicBallBody(),
       ),
     );
+  }
+}
+
+class MagicBallBody extends StatefulWidget {
+  @override
+  _MagicBallBodyState createState() => _MagicBallBodyState();
+}
+
+class _MagicBallBodyState extends State<MagicBallBody> {
+  int BallNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: (){
+          setState(() {
+            BallNumber = Random().nextInt(5)+1;
+          });
+        },
+        child: Image.asset('images/ball$BallNumber.png'),
+      ),
+    );
+  }
+}
